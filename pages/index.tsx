7ad2e30eb714fa00c22e66534fc82019b396eb8a
tedlittledale <ted.littledale@gmail.com>
2023-09-13 12:26:15 +0100

    hardcode deployment url

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,15 +11,7 @@ export default function Page() {
         <meta name="og:description" content="Vercel Edge Network" />
         <meta
           name="og:image"
-          content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
-            `${
-              process.env.VERCEL_URL ? "https://" + process.env.VERCEL_URL : ""
-            }/api/hubbub?groupname=${groupname}&groupsubname=${groupsubname}&image=${image}`
-          }
+          content={`https://vercel-og-nextjs-flame-five.vercel.app/api/hubbub?groupname=${groupname}&groupsubname=${groupsubname}&image=${image}`}
         />
       </Head>
       <h1>Demoing Dynamic Open Graph Images</h1>
