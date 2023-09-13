64a801590e7b048fbd2851434f58882e0c773428
tedlittledale <ted.littledale@gmail.com>
2023-09-13 13:59:57 +0100

    shorer url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
 import Head from "next/head";
 
+const groupHash = {
+  waterloo: {
+    groupname: "Waterloo Community Fridge",
+    groupsubname: "Oasis Community Hub : Waterloo",
+    image:
+      "https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Ftimetospare-public%2Fo%2FeventImages%2FdXRFzRQ7vrcdZdxXMdoG%2FComFridge.JPG%3Falt%3Dmedia£token%3Db3d4ae3d-f603-4c2d-b5b1-17819e1a3017",
+  },
+  chelsea: {
+    image:
+      "https://static.wixstatic.com/media/8b0461_1864699013994bbb9baa8c43bf38946cf000.jpg/v1/fill/w_479,h_271,al_c,q_80,usm_0.33_1.00_0.00,enc_auto/8b0461_1864699013994bbb9baa8c43bf38946cf000.jpg",
+    groupname: "Day centre for homeless and vulnerable adults",
+    groupsubname: "CHELSEA METHODIST CHURCH AND PASTORAL CENTRE",
+  },
+};
+
 export default function Page(props) {
   const { groupname, groupsubname, image } = props;
-
+  console.log({ groupname, groupsubname, image });
   return (
     <>
       <Head>
-        <meta name="og:title" content="Vercel Edge Network" />
-        <meta name="og:description" content="Vercel Edge Network" />
+        <meta name="og:title" content={`${groupname}`} />
+        <meta name="og:description" content={`${groupsubname}`} />
         <meta
           name="og:image"
           content={`https://vercel-og-nextjs-flame-five.vercel.app/api/hubbub?groupname=${groupname}&groupsubname=${groupsubname}&image=${image}`}
@@ -29,8 +44,9 @@ export default function Page(props) {
 export function getServerSideProps(context) {
   // Retrieve id
   const { query } = context;
-  const { groupname, groupsubname, image } = query;
-  console.log({ groupname, groupsubname, image });
+  const { name } = query;
+  console.log("name", name);
+  const { groupname, groupsubname, image } = groupHash[name];
 
   return {
     props: { groupname, groupsubname, image },
