9cfae174cc84231a3158de44f4e865c9113531af
tedlittledale <ted.littledale@gmail.com>
2023-09-13 12:35:09 +0100

    use server side props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
 
-export default function Page() {
-  const router = useRouter();
-  const { groupname, groupsubname, image } = router.query;
+export default function Page(props) {
+  const { groupname, groupsubname, image } = props;
+
   return (
-    <div>
+    <>
       <Head>
         <meta name="og:title" content="Vercel Edge Network" />
         <meta name="og:description" content="Vercel Edge Network" />
@@ -14,14 +13,26 @@ export default function Page() {
           content={`https://vercel-og-nextjs-flame-five.vercel.app/api/hubbub?groupname=${groupname}&groupsubname=${groupsubname}&image=${image}`}
         />
       </Head>
-      <h1>Demoing Dynamic Open Graph Images</h1>
-      <h2>{groupname}</h2>
-      <h3>{groupsubname}</h3>
+      <div>
+        <h1>Demoing Dynamic Open Graph Images</h1>
+        <h2>{groupname}</h2>
+        <h3>{groupsubname}</h3>
 
-      <img
-        src={`/api/hubbub?groupname=${groupname}&groupsubname=${groupsubname}&image=${image}`}
-        alt="Vercel Edge Network"
-      />
-    </div>
+        <img
+          src={`/api/hubbub?groupname=${groupname}&groupsubname=${groupsubname}&image=${image}`}
+          alt="Vercel Edge Network"
+        />
+      </div>
+    </>
   );
+}
+export function getServerSideProps(context) {
+  // Retrieve id
+  const { query } = context;
+  const { groupname, groupsubname, image } = query;
+  console.log({ groupname, groupsubname, image });
+
+  return {
+    props: { groupname, groupsubname, image },
+  };
 }
